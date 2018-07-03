789ef7465da70fad115b47c24ea3fa690fc2f6ed
Andy Ayers <andya@microsoft.com>
2018-07-03 10:47:06 -0700

    16K size

--- src/jit/alloc.h
+++ src/jit/alloc.h
@@ -47,7 +47,7 @@ private:
     // Thus if we want to make this smaller, we need to do a reserve / commit scheme
     enum
     {
-        DEFAULT_PAGE_SIZE = 16 * OS_page_size,
+        DEFAULT_PAGE_SIZE = 4 * OS_page_size,
     };
 
     class SinglePagePool;
