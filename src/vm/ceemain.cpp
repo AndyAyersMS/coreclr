f1354eed01d674fce3c8879c6afe255abe2ff045
David Wrighton <davidwr@microsoft.com>
2018-10-08 12:31:23 -0700

    Add new apis to LoaderAllocator for speculative virtual optimization - These api's both produce a result as well as a condition   that can be checked in the future to see if the result holds true - In some cases the condition cannot be checked via flag at this time - There are 3 apis   - DoesAnyTypeOverrideVTableSlot   - FindUniqueConcreteTypeWhichWithTypeInTypeHierarchy   - FindUniqueConcreteTypeWhichImplementsThisInterface - Currently this produces a derived type graph   - It is complete for classes/structs at this time, although its likely that the derived type graph of System.Object and System.ValueType isn't useful.   - For interfaces it attempts to only record 1 interface implementation

--- src/vm/ceemain.cpp
+++ src/vm/ceemain.cpp
@@ -699,6 +699,8 @@ void EEStartupHelper(COINITIEE fFlags)
 
         Frame::Init();
 
+        LoaderAllocator::Init();
+
 #ifdef FEATURE_TESTHOOKS
         IfFailGo(CLRTestHookManager::CheckConfig());
 #endif
