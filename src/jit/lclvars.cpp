2f2d103143c4c942d77cd69be785b9bdf0919eab
Andy Ayers <andya@microsoft.com>
2019-11-27 12:56:45 -0800

    Disable promotion for OSR locals
    Seems like it is difficult to allow promotion of OSR local structs
    as we don't know what the impact of the un-imported IL is, and we
    don't have struct liveness. So, just disable such locals from being
    promoted.

--- src/jit/lclvars.cpp
+++ src/jit/lclvars.cpp
@@ -1848,6 +1848,12 @@ bool Compiler::StructPromotionHelper::CanPromoteStructVar(unsigned lclNum)
         return false;
     }
 
+    if (compiler->lvaIsOSRLocal(lclNum))
+    {
+        JITDUMP("  struct promotion of V%02u is disabled because it is an OSR local\n", lclNum);
+        return false;
+    }
+
     CORINFO_CLASS_HANDLE typeHnd = varDsc->lvVerTypeInfo.GetClassHandle();
     return CanPromoteStructType(typeHnd);
 }
